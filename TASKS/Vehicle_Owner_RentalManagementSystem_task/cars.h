1e22951a0bdb0b064c844f7175aa6501b08cc87d
Pallavi T <pallavit8046@gmail.com>
2025-04-10 10:33:59 +0530

    owner-driver relationship

--- TASKS/Vehicle_Owner_RentalManagementSystem_task/cars.h
+++ TASKS/Vehicle_Owner_RentalManagementSystem_task/cars.h
@@ -1,18 +1,19 @@
 #ifndef CARS_H
 #define CARS_H
 #include "drivers.h"
+
 class Cars
 {
+private:
+    Drivers *m_driver;
+    std::string m_brand;
 public:
     Cars();
     ~Cars();
 
-    void setDriver(Drivers* d);
+    void setDriver(Drivers *d);
     std::string getBrand();
     void setBrand(std::string newBrand);
-private:
-    Drivers* m_driver;
-    std::string m_brand;
 };
 
-#endif // CAR_H
+#endif
